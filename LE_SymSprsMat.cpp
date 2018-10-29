f99ee393134f8f283c6c87317a10d321a7a02265
xpeng2333 <2544627510@qq.com>
2018-10-29 16:39:14 +0800

    506

--- LE_SymSprsMat.cpp
+++ LE_SymSprsMat.cpp
@@ -504,9 +504,9 @@ void LU_NumbericSymG(SprsMatRealStru *pG, SprsUMatRealStru *pFU) {
 
         if (kp == 0) {
             dk = g[jmn];
-            if (dk >= 0.0 && fabs(dk) < 1.0e-20)
+            if (dk >= 0.0 && dk < 1.0e-20)
                 dk = 1.0e-20;
-            if (dk < 0.0 && fabs(dk) < 1.0e-20)
+            if (dk < 0.0 && dk > -1.0e-20)
                 dk = -1.0e-20;
             d_u[i] = dk;
             continue;
